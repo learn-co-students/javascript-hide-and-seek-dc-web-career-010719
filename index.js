3e81c4d71030dba77681dbf3a35e7738337f5256
Serven Maraghi <maraghiserven@gmail.com>
2019-02-20 13:12:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector) {
+    return document.querySelector(selector)
+}
 
+function nestedTarget() {
+    return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+    const list = document.querySelectorAll('.ranked-list')
+
+    for (let i = 0; i < list.length; i++) {
+        list[i].innerHTML = parseInt(list[i].innerHTML + n)
+    }
+}
+
+function deepestChild() {
+    const node = document.getElementById('grand-node').querySelectorAll('div')
+        return node[node.length-1]
+}

1360d04152f4d1f6f3534f8bfcf8185caf241cb5
github username <github email address>
2019-02-20 13:19:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function getFirstSelector(selector) {
+    return document.querySelector(selector)
+}
 
+function nestedTarget() {
+    return document.querySelector("#nested .target")
+}
+
+function deepestChild() {
+    let layer1 = document.querySelector("div#grand-node")
+    let layer2 = layer1.children[0]
+    while (layer2) {
+        layer1 = layer2
+        layer2 = layer1.children[0]
+    }
+
+    return layer1
+}
+
+function increaseRankBy(n) {
+    let lists = document.querySelectorAll(".ranked-list")
+    for (const list of lists) {
+        let children = list.children
+        for (let child of children) {
+            num = parseInt(child.innerText, 10)
+            child.innerText = num + n
+        }
+    }
+}

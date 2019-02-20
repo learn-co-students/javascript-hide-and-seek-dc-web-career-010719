9d00045abed2f759049354e58d1dfc0fe484e238
github username <metelskyi88@gmail.com>
2019-02-20 16:46:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+function getFirstSelector(selector){
+  let result = document.querySelector(selector)
+  return result
+}
+function nestedTarget(){
+  let result = document.querySelector('.target')
+  return result
+}
+
+function deepestChild(){
+  let result = document.querySelector('#grand-node div div div div')
+  return result
+}
+
+function increaseRankBy(n){
+  const list = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = list.length; i < l; i++) {
+      let children = list[i].children;
+
+      for (let j = 0, k = children.length; j < k; j++) {
+        children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+      }
+    }
+  }

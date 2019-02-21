74835b6505aafea9c7e7e7b72f1cf8a6f5d67f39
Heloise Taillet <heloise96@hotmail.com>
2019-02-20 19:59:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector(`#nested .target`);
+}
+
+function increaseRankBy(n){
+  for(const list of document.querySelectorAll(`.ranked-list`)) {
+    for(const child of Array.from(list.children)) {
+      child.innerHTML = parseInt(child.innerHTML) + n}}
+}
+
+function deepestChild() {
+  let outer = document.querySelector(`#grand-node`)
+  let inner = outer.children[0];
+
+  while (inner) {
+    outer = inner;
+    inner = outer.children[0];
+  }
+
+  return outer;
+}
+
+
+function deepIterator (target) {
+  if (typeof target === 'object') {
+    for (const key in target) {
+      deepIterator(target[key]);
+    }
+  } else {
+    console.log(target);
+  }
+}
